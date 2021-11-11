3fec760661fca4434d984d8b58ef3bf733ded412
likhith00 <likhith.prudhivi@gmail.com>
2021-11-11 19:07:01 +0530

    prediction.py added

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import os
 import yaml
 import joblib
 import numpy as np
-
+from prediction_service import prediction
 
 params_path = "params.yaml"
 webapp_root = "webapp"
@@ -43,12 +43,11 @@ def index():
     if request.method == "POST":
         try:
             if request.form:
-                data = dict(request.form).values()
-                data = [list(map(float,data))]
-                response = predict(data)
+                data_req = dict(request.form)
+                response = prediction.form_response(data_req)
                 return render_template("index.html",response=response)
             elif request.json:
-                response = api_response(request)
+                response = prediction.api_response(request.json)
                 return jsonify(response)
         except Exception as e:
             print(e)
