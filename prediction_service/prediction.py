ef0ed7748e0a8c989d7f616427c5c045e95cb67b
likhith00 <likhith.prudhivi@gmail.com>
2021-11-11 19:15:31 +0530

    exceptions updated

--- prediction_service/prediction.py
+++ prediction_service/prediction.py
@@ -4,18 +4,21 @@ import json
 import joblib
 import numpy as np
 
+
 params_path = "params.yaml"
-schema_path = os.path.join("prediction_service","schema_in.json")
+schema_path = os.path.join("prediction_service", "schema_in.json")
 
 class NotInRange(Exception):
-    def __init__(self,message="Values entered are not in range"):
+    def __init__(self, message="Values entered are not in expected range"):
         self.message = message
         super().__init__(self.message)
 
 class NotInCols(Exception):
-    def __init__(self,message="Not in Columns"):
+    def __init__(self, message="Not in cols"):
         self.message = message
         super().__init__(self.message)
+
+
 
 def read_params(config_path=params_path):
     with open(config_path) as yaml_file:
@@ -26,8 +29,7 @@ def predict(data):
     config = read_params(params_path)
     model_dir_path = config["webapp_model_dir"]
     model = joblib.load(model_dir_path)
-    prediction = model.predict(data).to_list()[0]
-
+    prediction = model.predict(data).tolist()[0]
     try:
         if 3 <= prediction <= 8:
             return prediction
@@ -35,6 +37,7 @@ def predict(data):
             raise NotInRange
     except NotInRange:
         return "Unexpected result"
+
 
 def get_schema(schema_path=schema_path):
     with open(schema_path) as json_file:
@@ -47,30 +50,44 @@ def validate_input(dict_request):
         actual_cols = schema.keys()
         if col not in actual_cols:
             raise NotInCols
-    def _validate_values(col,val):
+
+    def _validate_values(col, val):
         schema = get_schema()
-        if not (schema[col]["min"]<=float(dict_request[col])<=schema[col]["max"]):
+
+        if not (schema[col]["min"] <= float(dict_request[col]) <= schema[col]["max"]) :
             raise NotInRange
 
-    for col,val in dict_request.items():
+    for col, val in dict_request.items():
         _validate_cols(col)
-        _validate_values(col,val)
+        _validate_values(col, val)
+    
     return True
+
 
 def form_response(dict_request):
     if validate_input(dict_request):
         data = dict_request.values()
-        data = [list(map(float,data))]
+        data = [list(map(float, data))]
         response = predict(data)
-        return response 
+        return response
 
 def api_response(dict_request):
     try:
-        if validate_input(dict_request=True):
+        if validate_input(dict_request):
             data = np.array([list(dict_request.values())])
             response = predict(data)
             response = {"response": response}
             return response
+            
+    except NotInRange as e:
+        response = {"the_exected_range": get_schema(), "response": str(e) }
+        return response
+
+    except NotInCols as e:
+        response = {"the_exected_cols": get_schema().keys(), "response": str(e) }
+        return response
+
+
     except Exception as e:
-        response = {"the_expected_range":get_schema(),"response":str(e)}
+        response = {"response": str(e) }
         return response
